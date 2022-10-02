6f6903e737b1cee4065f7b90155cb472be5e636f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-10-02 17:04:47 +0530

    add some Movie

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 type Movie struct {
 	ID       string    `json:"id"`
 	Isbn     string    `json:"isbn"`
+	Title     string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
@@ -22,6 +23,22 @@ type Director struct {
 var movies []Movie
 
 func main() {
+	movies = append(movies, Movie{
+		ID: "1",
+		Isbn: "1234",
+		Director: &Director{
+			FirstName: "Abhisek",
+			LastName: "Gupta",
+		},
+	})
+	movies = append(movies, Movie{
+		ID: "2",
+		Isbn: "1235",
+		Director: &Director{
+			FirstName: "Yam",
+			LastName: "Joram",
+		},
+	})
 	r := mux.NewRouter()
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
